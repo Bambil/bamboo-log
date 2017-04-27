3bb0c127c0c078a0b3cc55d0549b2204d982c5c1
Parham Alvani <parham.alvani@gmail.com>
2017-04-27 22:15:11 +0430

    Discovery module was improved.

--- discovery/index.js
+++ discovery/index.js
@@ -1,13 +1,21 @@
 'use strict';
 
-module.exports = function (options) {
+var Agent = require('../models/agent');
+
+module.exports = function discovery(options) {
   /**
-   * Fetch the list of all the products.
+   * Fetch the list of all the agents.
    */
   this.add({role: 'discovery', action: 'fetch'}, function (msg, respond) {
     var agents = this.make('agents');
   });
 
+  /**
+   * Ping from agnet.
+   */
   this.add({role: 'discovery', action: 'ping'}, function (msg, respond) {
+    var newAgent = new Agent(msg.data.id);
+    console.log(newAgent);
+    var agents = this.make("agents");
   });
 };
