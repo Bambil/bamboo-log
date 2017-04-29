4754c45f88cfcdb5b9da37573e82ae0243ae6505
Parham Alvani <parham.alvani@gmail.com>
2017-04-29 20:41:51 +0430

    Agent module was improved.

--- agent/index.js
+++ agent/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const winston = require('winston');
+
 module.exports = function agent(options) {
   /**
    * Fetch the list of all the agents.
@@ -14,15 +16,24 @@ module.exports = function agent(options) {
    */
   this.add({role: 'agent', action: 'ping'}, function (msg, respond) {
     let agents = this.make('I1820', 'agent');
+
     agents.list$({uuid: msg.data.id}, function (err, list) {
       if (list.length === 0) {
+
         agents.uuid = msg.data.id;
         agents.things = msg.data.things;
         agents.timestamp = Date.now();
+
+        winston.info(agents.data$(false));
+
         agents.save$(respond);
       } else if (list.length === 1) {
+
         let agent = list[0];
         agent.timestamp = Date.now();
+
+        winston.info(agent.data$(false));
+
         agent.save$(respond);
       } else {
         respond(new Error(`Duplicate agent: ${msg.data.id}`), null);
