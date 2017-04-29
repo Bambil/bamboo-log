2c5195637164884817d30ade52707422649e9879
Parham Alvani <parham.alvani@gmail.com>
2017-04-29 19:17:44 +0430

    Discovery module was improved.

--- discovery/index.js
+++ discovery/index.js
@@ -5,14 +5,19 @@ module.exports = function discovery(options) {
    * Fetch the list of all the agents.
    */
   this.add({role: 'discovery', action: 'fetch'}, function (msg, respond) {
-    var agents = this.make('agents');
+    let agents = this.make('I1820', 'discovery', 'agent');
+    agents.list$({}, function (err, list) {
+      list.forEach(function (agent) {
+        console.log(agent);
+      });
+    });
   });
 
   /**
    * Ping from agnet.
    */
   this.add({role: 'discovery', action: 'ping'}, function (msg, respond) {
-    var agents = this.make("agent");
+    let agents = this.make('I1820', 'discovery', 'agent');
     agents.load$(msg.data.id, function (err, agent) {
       if (agent === null) {
         agents.id = msg.data.id;
