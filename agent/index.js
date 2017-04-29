bf7aec945f01fde0eb4aeed2d87ebf163bb609fc
Parham Alvani <parham.alvani@gmail.com>
2017-04-29 19:20:44 +0430

    Discovery module name was changed into agent.

--- agent/index.js
+++ agent/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports = function discovery(options) {
+module.exports = function agent(options) {
   /**
    * Fetch the list of all the agents.
    */
-  this.add({role: 'discovery', action: 'fetch'}, function (msg, respond) {
-    let agents = this.make('I1820', 'discovery', 'agent');
+  this.add({role: 'agent', action: 'fetch'}, function (msg, respond) {
+    let agents = this.make('I1820', 'agent');
     agents.list$({}, function (err, list) {
       list.forEach(function (agent) {
         console.log(agent);
@@ -16,8 +16,8 @@ module.exports = function discovery(options) {
   /**
    * Ping from agnet.
    */
-  this.add({role: 'discovery', action: 'ping'}, function (msg, respond) {
-    let agents = this.make('I1820', 'discovery', 'agent');
+  this.add({role: 'agent', action: 'ping'}, function (msg, respond) {
+    let agents = this.make('I1820', 'agent');
     agents.load$(msg.data.id, function (err, agent) {
       if (agent === null) {
         agents.id = msg.data.id;
