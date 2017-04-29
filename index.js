d1272b3ec1456ebc2a2212f248c14bb5ca37e923
Parham Alvani <parham.alvani@gmail.com>
2017-04-29 20:42:03 +0430

    Main project entry point was improved.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = require('seneca')();
 const mqtt = require('mqtt');
 const config = require('config');
 const entities = require('seneca-entity');
+const winston = require('winston');
+
+/**
+ * Configure CLI output on the default logger
+ */
+winston.cli();
 
 /**
  * I1820 Initiation
@@ -14,7 +20,7 @@ console.log(' * 18.20 at Sep 07 2016 7:20 IR721');
  * Initiates seneca modules
  */
 app.use(entities);
-app.use('discovery');
+app.use('agent');
 app.use('log');
 
 /**
@@ -24,7 +30,8 @@ const mqttClient  = mqtt.connect(`mqtt://${config.get('mqtt.ip')}`);
 
 mqttClient.on('connect', function () {
   console.log(`* MQTT at ${config.get('mqtt.ip')}`);
-  mqttClient.subscribe(`I1820/${config.get('cluster.name')}/discovery/ping`);
+  mqttClient.subscribe(`I1820/${config.get('cluster.name')}/agent/ping`);
+  mqttClient.subscribe(`I1820/${config.get('cluster.name')}/agent/fatch`);
   mqttClient.subscribe(`I1820/${config.get('cluster.name')}/log/send`);
 });
 
@@ -33,12 +40,5 @@ mqttClient.on('error', function () {
 
 mqttClient.on('message', function (topic, message) {
   let splitedTopic = topic.split('/');
-  app.act({role: splitedTopic[2], action: splitedTopic[3], data: JSON.parse(message)},
-          function (err, data) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log(data);
-            }
-          });
+  app.act({role: splitedTopic[2], action: splitedTopic[3], data: JSON.parse(message)});
 });
