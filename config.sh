6debc5823b3f17667770d41f7b727b1541d470a2
Parham Alvani <parham.alvani@gmail.com>
2017-04-30 10:22:40 +0430

    Configuration creator script was added.

---
+++ config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -r -d '' config << EOF
+{
+  "mqtt": {
+    "ip": "$MQTT_IP",
+    "port": "$MQTT_PORT"
+  },
+  "influx": {
+    "host": "$INFLUX_HOST",
+    "database": "$INFLUX_DB"
+  },
+  "cluster": {
+    "name": "$CLUSTER_NAME"
+  }
+}
+EOF
+
+echo $config > config/default.json
