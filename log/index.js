763eba5cba15b313b5e4ce9f9058f6927cb5d6bb
Parham Alvani <parham.alvani@gmail.com>
2017-04-30 07:39:26 +0430

    Log module was improved.

--- log/index.js
+++ log/index.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const config = require('config');
-const influx = require('influx');
+const Influx = require('influx');
+
+const influx = new Influx.InfluxDB({
+});
 
 module.exports = function log(options) {
+  this.add({role: 'log', action: 'send'}, function (msg, respond) {
+    let points = [];
+    msg.data.states.forEach(function (state) {
+      points.push({
+        measurement: state.name,
+        tags: {agent_id: msg.data.agent, device_id: msg.data.device},
+        fields: {value: state.value}
+      });
+    });
+    influx.writePoints(points).then(respond);
+  });
 };
