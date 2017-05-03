a82c2071a47955c3bc3a840d871107d9d01216c4
Parham Alvani <parham.alvani@gmail.com>
2017-05-03 07:04:29 +0430

    Route module export was corrected.

--- http/routes.js
+++ http/routes.js
@@ -1,9 +1,11 @@
-const routes = [{
-  pin: {role: 'agent', action: '*'},
-  prefix: '/core/api/v1/agent',
-  map: {
-    fetch: {
-      GET: true
+module.exports = {
+  routes: [{
+    pin: {role: 'agent', action: '*'},
+    prefix: '/v1/agent',
+    map: {
+      fetch: {
+        GET: true
+      }
     }
-  }
-}];
+  }]
+};
