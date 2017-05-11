49f3f4a42a95af70f1875da9b0d2204e4470b40b
Parham Alvani <parham.alvani@gmail.com>
2017-05-11 19:46:25 +0430

    Better code Better life :P

--- http/index.js
+++ http/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const routes = require('./routes');
 const hapi = require('hapi');
 
 module.exports = {
-  routes: routes,
+  routes: require('./routes'),
   adapter: require('seneca-web-adapter-hapi'),
   context: (() => {
-    let server = new hapi.Server();
+    const server = new hapi.Server();
     server.connection({port: 8080});
     return server;
   })()
