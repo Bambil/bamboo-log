9218c3e32c8a956e998121330aa3dfd14ccc2294
Parham Alvani <parham.alvani@gmail.com>
2017-05-11 20:01:47 +0430

    Better code Better life :P

--- agent/index.js
+++ agent/index.js
@@ -3,12 +3,12 @@
 const winston = require('winston');
 
 /* Substack Pattern */
-module.exports = function agent(options) {
+module.exports = function agent() {
   /**
    * Fetch the list of all the agents.
    */
-  this.add({role: 'agent', action: 'fetch'}, function (args, respond) {
-    let agents = this.make('I1820', 'agent');
+  this.add({role: 'agent', action: 'fetch'}, (args, respond) => {
+    const agents = this.make('I1820', 'agent');
     winston.info('I1820.agent fetch:');
     agents.list$({}, respond);
   });
@@ -16,8 +16,9 @@ module.exports = function agent(options) {
   /**
    * Fetch an agent by id.
    */
-  this.add({role: 'agent', action: 'fetch', criteria: "byUUID"}, function (args, respond) {
-    let agents = this.make('I1820', 'agent');
+  this.add({role: 'agent', action: 'fetch', criteria: 'byUUID'},
+           (args, respond) => {
+    const agents = this.make('I1820', 'agent');
     winston.info('I1820.agent fetch byUUID:');
     agents.list$({uuid: args.uuid}, respond);
   });
@@ -25,11 +26,11 @@ module.exports = function agent(options) {
   /**
    * Ping from agnet.
    */
-  this.add({role: 'agent', action: 'ping'}, function (msg, respond) {
-    let agents = this.make('I1820', 'agent');
+  this.add({role: 'agent', action: 'ping'}, (msg, respond) => {
+    const agents = this.make('I1820', 'agent');
     winston.info('I1820.agent ping:');
 
-    agents.list$({uuid: msg.data.id}, function (err, list) {
+    agents.list$({uuid: msg.data.id}, (err, list) => {
       if (list.length === 0) {
 
         agents.uuid = msg.data.id;
@@ -38,17 +39,17 @@ module.exports = function agent(options) {
 
         winston.info(agents.data$(false));
 
-        agents.save$(function (err, agent) {
+        agents.save$((err, agent) => {
           respond(err, agent.data$(false));
         });
       } else if (list.length === 1) {
 
-        let agent = list[0];
+        const agent = list[0];
         agent.timestamp = Date.now();
 
         winston.info(agent.data$(false));
 
-        agents.save$(function (err, agent) {
+        agents.save$((err, agent) => {
           respond(err, agent.data$(false));
         });
       } else {
