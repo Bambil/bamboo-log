5cbdac5241d9b16985648eeb6ace861e2604a490
Parham Alvani <parham.alvani@gmail.com>
2017-05-15 22:34:40 +0430

    Connectivity module was improved.

--- connectivity/index.js
+++ connectivity/index.js
@@ -2,25 +2,28 @@
 
 const mqtt = require('mqtt');
 const config = require('config');
+const winston = require('winston');
 
 /**
  * Creates MQTT connection
  */
 const mqttClient  = mqtt.connect(`mqtt://${config.get('mqtt.ip')}`);
 
-mqttClient.on('connect', function () {
-  console.log(` * MQTT at ${config.get('mqtt.ip')}`);
+mqttClient.on('connect',() => {
+  winston.log(` * MQTT at ${config.get('mqtt.ip')}`);
   mqttClient.subscribe(`I1820/${config.get('cluster.name')}/agent/ping`);
   mqttClient.subscribe(`I1820/${config.get('cluster.name')}/agent/fatch`);
   mqttClient.subscribe(`I1820/${config.get('cluster.name')}/log/send`);
 });
 
-module.exports = function connectivity(options) {
-  mqttClient.on('error', () => {
+module.exports = function connectivity() {
+  mqttClient.on('error', (err) => {
+    winston.error(err);
   });
 
   mqttClient.on('message', (topic, message) => {
-    let splitedTopic = topic.split('/');
-    this.act({role: splitedTopic[2], action: splitedTopic[3], data: JSON.parse(message)});
+    const splitedTopic = topic.split('/');
+    this.act({role: splitedTopic[2], action: splitedTopic[3],
+             data: JSON.parse(message)});
   });
 };
