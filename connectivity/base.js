e6827ebdde1aae63c6b5f872b85b62ebd66e9e5d
Parham Alvani <parham.alvani@gmail.com>
2017-06-04 19:35:15 +0430

    Adds some default method to I1820Connectivity.

--- connectivity/base.js
+++ connectivity/base.js
@@ -11,6 +11,22 @@ const EventEmitter = require('events').EventEmitter;
 
 
 class I1820Connectivity extends EventEmitter {
+
+  onLog(msg) {
+    this.emit('log', msg);
+  }
+
+  onPing(msg) {
+    this.emit('ping', msg);
+  }
+
+  onConfiguration(msg) {
+    this.emit('configuration', msg);
+  }
+
+  onTrap(msg) {
+    this.emit('trap', msg);
+  }
 }
 
 module.exports = I1820Connectivity;
