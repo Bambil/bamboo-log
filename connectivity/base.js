92e678c1f5b91fcdff8e4f180a470eed163a7e5c
Parham Alvani <parham.alvani@gmail.com>
2017-06-04 13:32:46 +0430

    I1820 connectivity base was added.

---
+++ connectivity/base.js
@@ -0,0 +1,16 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 04-06-2017
+ * |
+ * | File Name:     base.js
+ * +===============================================
+ */
+const EventEmitter = require('events').EventEmitter;
+
+
+class I1820Connectivity extends EventEmitter {
+}
+
+module.exports = I1820Connectivity;
