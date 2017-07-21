8ea0e7ebc07aa67a77e21fecfebd4111b76db6ad
Parham Alvani <parham.alvani@gmail.com>
2017-07-21 23:06:53 +0430

    Component manager was improved.

--- src/component.js
+++ src/component.js
@@ -35,7 +35,7 @@ class I1820Component extends EventEmitter {
       let result = topic.match(/^I1820\/(\w+)/i)
       if (result && result.length === 2) {
         let action = result[1]
-        this.emit(action, JSON.parse(message.payload))
+        this.emit(action, JSON.parse(message))
       }
     })
   }
