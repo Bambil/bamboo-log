3ab5bfa586797f3b638a1695bf6fad0473c431a1
Parham Alvani <parham.alvani@gmail.com>
2017-07-29 00:25:08 +0430

    Makes everything bamboo !

--- index.js
+++ index.js
@@ -8,28 +8,28 @@
  * +===============================================
  */
 /* Configuration */
-if (!process.env.I1820_CONNECTIVITY_HOST) {
-  process.env.I1820_CONNECTIVITY_HOST = '127.0.0.1'
+if (!process.env.BAMBOO_CONNECTIVITY_HOST) {
+  process.env.BAMBOO_CONNECTIVITY_HOST = '127.0.0.1'
 }
 
-if (!process.env.I1820_CONNECTIVITY_PORT) {
-  process.env.I1820_CONNECTIVITY_PORT = 1883
+if (!process.env.BAMBOO_CONNECTIVITY_PORT) {
+  process.env.BAMBOO_CONNECTIVITY_PORT = 1883
 }
 
-if (!process.env.I1820_HTTP_HOST) {
-  process.env.I1820_HTTP_HOST = '0.0.0.0'
+if (!process.env.BAMBOO_HTTP_HOST) {
+  process.env.BAMBOO_HTTP_HOST = '0.0.0.0'
 }
 
-if (!process.env.I1820_HTTP_PORT) {
-  process.env.I1820_HTTP_PORT = 8080
+if (!process.env.BAMBOO_HTTP_PORT) {
+  process.env.BAMBOO_HTTP_PORT = 8080
 }
 
-if (!process.env.I1820_DATABASE_NAME) {
-  process.env.I1820_DATABASE_NAME = 'I1820'
+if (!process.env.BAMBOO_DATABASE_NAME) {
+  process.env.BAMBOO_DATABASE_NAME = 'Bamboo'
 }
 
-if (!process.env.I1820_DATABASE_HOST) {
-  process.env.I1820_DATABASE_HOST = '127.0.0.1'
+if (!process.env.BAMBOO_DATABASE_HOST) {
+  process.env.BAMBOO_DATABASE_HOST = '127.0.0.1'
 }
 
 /* winston.js */
@@ -39,27 +39,27 @@ const winston = require('winston')
 winston.cli()
 winston.info(' * 18.20 at Sep 07 2016 7:20 IR721')
 
-/* I1820 Log Initiation */
-const I1820Log = require('./src/log')
+/* Bamboo Log Initiation */
+const BambooLog = require('./src/log')
 
-const i1820Log = new I1820Log({
-  database: process.env.I1820_DATABASE_NAME,
-  host: process.env.I1820_DATABASE_HOST
+const bambooLog = new BambooLog({
+  database: process.env.BAMBOO_DATABASE_NAME,
+  host: process.env.BAMBOO_DATABASE_HOST
 })
 
-/* I1820 component initiation */
-const I1820Component = require('@ibamboo/component')
+/* Bamboo component initiation */
+const BambooComponent = require('@ibamboo/component')
 
-new I1820Component({
-  mqttHost: process.env.I1820_CONNECTIVITY_HOST,
-  mqttPort: process.env.I1820_CONNECTIVITY_PORT,
+new BambooComponent({
+  mqttHost: process.env.BAMBOO_CONNECTIVITY_HOST,
+  mqttPort: process.env.BAMBOO_CONNECTIVITY_PORT,
   name: 'log',
   subscribes: ['log']
 }).on('ready', () => {
-  winston.info(` * MQTT at ${process.env.I1820_CONNECTIVITY_HOST}:${process.env.I1820_CONNECTIVITY_PORT}`)
+  winston.info(` * MQTT at ${process.env.BAMBOO_CONNECTIVITY_HOST}:${process.env.BAMBOO_CONNECTIVITY_PORT}`)
 }).on('log', (message) => {
   winston.data(message)
-  i1820Log.log(`${message.tenant}/${message.name}`, message.data.id, message.data.timestamp, message.data.state)
+  bambooLog.log(`${message.tenant}/${message.name}`, message.data.id, message.data.timestamp, message.data.state)
 })
 
 /* HTTP server initiation */
@@ -68,8 +68,8 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 
 server.connection({
-  host: process.env.I1820_HTTP_HOST,
-  port: process.env.I1820_HTTP_PORT
+  host: process.env.BAMBOO_HTTP_HOST,
+  port: process.env.BAMBOO_HTTP_PORT
 })
 
 server.route({
@@ -82,7 +82,7 @@ server.route({
   },
   handler: function (request, reply) {
     for (let s of request.payload['states']) {
-      i1820Log.fetch(request.payload['agent_id'], request.payload['thing_id'], s)
+      bambooLog.fetch(request.payload['agent_id'], request.payload['thing_id'], s)
     }
     return reply('hello world')
   }
