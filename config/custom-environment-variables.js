f1b083a84b07c3672856044827793440a52520ba
Parham Alvani <parham.alvani@gmail.com>
2017-08-10 19:21:24 +0430

    Configuration was added.

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,14 @@
+module.exports = {
+  datbase: {
+    name: 'BAMBOO_DATABASE_NAME',
+    host: 'BAMBOO_DATABASE_HOST'
+  },
+  connectivity: {
+    host: 'BAMBOO_CONNECTIVITY_HOST',
+    port: 'BAMBOO_CONNECTIVITY_PORT'
+  },
+  http: {
+    host: 'BAMBOO_HTTP_HOST',
+    port: 'BAMBOO_HTTP_PORT'
+  }
+}
---
+++ config/default.js
@@ -0,0 +1,14 @@
+module.exports = {
+  database: {
+    name: 'Bamboo',
+    host: '127.0.0.1'
+  },
+  connectivity: {
+    host: '127.0.0.1',
+    port: 1883
+  },
+  http: {
+    host: '0.0.0.0',
+    port: 8080
+  }
+}
