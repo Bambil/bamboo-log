6d500a11adb3a92052d4b4a52cf6bb999bde90b6
Parham Alvani <parham.alvani@gmail.com>
2017-09-01 00:51:04 +0430

    Bamboo log uses strategy pattern.

--- src/log.js
+++ src/log.js
@@ -7,33 +7,15 @@
  * | File Name:     log.js
  * +===============================================
  */
-const Influx = require('influx')
 const logger = require('./logger')
 
 class BambooLog {
-  constructor (options) {
-    this.influx = new Influx.InfluxDB({
-      host: options.host,
-      database: options.database
-    })
-
-    this.influx.createDatabase(options.database).then(() => {
-    }).catch((err) => {
-      logger.log('error', err)
-    })
+  constructor (strategy) {
+    this.strategy = strategy
   }
 
   log (agentId, thingId, timestamp, states) {
-    const points = []
-    for (let name in states) {
-      points.push({
-        measurement: name,
-        tags: {agentId: agentId, deviceId: thingId},
-        fields: {value: states[name]},
-        time: timestamp
-      })
-    }
-    this.influx.writePoints(points).then(() => {
+    this.strategy.log(agentId, thingId, timestamp, states).then(() => {
     }).catch((err) => {
       logger.log('error', err)
     })
@@ -43,23 +25,7 @@ class BambooLog {
     if (!number) {
       number = 1
     }
-    return new Promise((resolve, reject) => {
-      this.influx.query(`SELECT * FROM ${measurement}
-                  WHERE "agentId" = '${agentId}' AND "deviceId" = '${thingId}'
-                  ORDER BY time DESC LIMIT ${number};`)
-        .then((rows) => {
-          let states = []
-          for (let r of rows) {
-            states.push({
-              value: r.value,
-              time: r.time
-            })
-          }
-          resolve(states)
-        }).catch((err) => {
-          reject(err)
-        })
-    })
+    return this.strategy.fetch(agentId, thingId, measurement, number)
   }
 }
 
