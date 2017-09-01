90dd74b814f726938ca5033f4363a8db2ce9fc60
Parham Alvani <parham.alvani@gmail.com>
2017-09-01 16:23:51 +0430

    Adds cassandra provider.

--- src/cassandra.js
+++ src/cassandra.js
@@ -23,8 +23,35 @@ module.exports = function (options) {
 
   return {
     log: function (agentId, thingId, timestamp, states) {
+      for (let name in states) {
+        const query = `INSERT INTO ${name} (agnet_id, thing_id, timestamp, value) VALUES (?,?,?,?)`
+        client.execute(query, [agentId, thingId, timestamp, states[name]], {prepare: true}, (err) => {
+          if (err) {
+            logger.log('error', err)
+          }
+        })
+      }
     },
     fetch: function (agentId, thingId, measurement, number) {
+      return new Promise((resolve, reject) => {
+        client.execute(`SELECT * FROM ${measurement}
+                  WHERE "agentId" = '${agentId}' AND "deviceId" = '${thingId}'
+                  ORDER BY time DESC LIMIT ${number};`,
+          (err, result) => {
+            if (err) {
+              reject(err)
+            }
+            let rows = result.rows
+            let states = []
+            for (let r of rows) {
+              states.push({
+                value: r.value,
+                time: r.time
+              })
+            }
+            resolve(states)
+          })
+      })
     }
   }
 }
